145e3f2aa44c11af045da4f090952039bd306228
Ruchi Vishwakarma <122896816+ivy396@users.noreply.github.com>
2023-05-13 15:42:35 +0530

    first commit

---
+++ config/key.js
@@ -0,0 +1,4 @@
+module.exports = {
+    MongoURI:"mongodb+srv://sayanimanna0:XbsraczaoMSMzbBf@cluster0.loebm3g.mongodb.net/myFirstDatabase"
+    // "mongodb+srv://kumar:awxhnpt6Mf9keSRI@cluster0.zsmd20g.mongodb.net/?retryWrites=true&w=majority"
+}
